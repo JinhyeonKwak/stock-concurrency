9821c8c9b521389d28dcba742338bc6fd6d7b75f
jinhyeon <vmflwu123@gmail.com>
2024-09-21 21:12:07 +0900

    fix: 빠진 부분 수정

--- src/main/java/com/example/stock/facade/NamedLockStockFacade.java
+++ src/main/java/com/example/stock/facade/NamedLockStockFacade.java
@@ -2,6 +2,7 @@ package com.example.stock.facade;
 
 import com.example.stock.repository.LockRepository;
 import com.example.stock.service.StockService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +13,7 @@ public class NamedLockStockFacade {
     private final LockRepository lockRepository;
     private final StockService stockService;
 
+    @Transactional
     public void decrease(Long id, Long quantity) {
         try {
             lockRepository.getLock(id.toString());
